570ad1c819f9a970ff857d51d805028280fb0948
homay <bijay.workplace@gmail.com>
2019-01-11 10:59:49 +0545

    Uploading image in NodeJS with express-fileupload :

---
+++ source.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+const fileUpload = require('express-fileupload');
+//npm install ejs, express, express-fileupload
+
+//middleware
+app.use(express.static(__dirname));
+app.set('view engine', 'ejs');
+app.use(fileUpload());
+
+app.get('/inputFile', function(req, res){
+  res.render('inputt');
+});
+
+app.post('/upload', function(req, res) {
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+   var startup_image = req.files.foo;
+   var fileName = req.body.fileName;
+   
+   // Use the mv() method to place the file somewhere on your server
+   startup_image.mv(__dirname + '/images/' + fileName , function(err) {
+     if(err){
+       console.log(err);
+     }else{
+    console.log("uploaded");
+    res.send('uploaded');
+}
+   });
+ });
+
+app.listen(7777);
